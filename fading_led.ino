63d5f7bcbae2a8dcc34b13a04ebc6aef6d31a12c
Rushil Rawat <61906345+RushilRawat@users.noreply.github.com>
2020-12-28 20:01:50 +0530

    Update fading_led.ino
    Fading in/out an LED

--- fading_led.ino
+++ fading_led.ino
@@ -1,12 +1,3 @@
-/*
-   Fade
-   This example shows how to fade an LED on pin 9 using the analogWrite() function.
-
-   The analogWrite() function uses PWM, so if you want to change the pin you're using, be
-   sure to use another PWM capable pin. On most Arduino, the PWM pins are identified with
-   a "~" sign, like ~3, ~5, ~6, ~9, ~10 and ~11.
-*/
-
 int led = 9; // the PWM pin the LED is attached to
 int brightness = 0; // how bright the LED is
 int fadeAmount = 5; // how many points to fade the LED by
@@ -29,5 +20,6 @@ void loop() {
       fadeAmount = -fadeAmount ;
    }
    // wait for 30 milliseconds to see the dimming effect
-   delay(25); // you can make it a discolight by setting delay(1)
+   delay(25); 
+   // you can make it a discolight by setting delay(1)
 }
